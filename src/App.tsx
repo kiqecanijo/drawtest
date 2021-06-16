2fca1a8e51697b22acb1756f717b5dc85fcdf109
kiqecanijo <enry1375@gmail.com>
2021-06-16 00:22:52 -0500

    added transponse

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
+
+const { innerWidth: WIDTH, innerHeight: HEIGHT } = window
 
 const App = () => {
+  const [charts, setCharts] = useState([])
+
+  // 🖼 ref init
+  const canvasRef = useRef(null)
+
+  const mapBars = data => {
+    const dataSet: number[][] = Object.values(data)
+
+    const transponse = dataSet[0].map((_, colIndex) =>
+      dataSet.map(row => row[colIndex])
+    )
+    setCharts(transponse)
+  }
+
   useEffect(() => {
     fetch('https://landing.backend.mivest.io/challenge')
       .then(res => res.json())
-      .then(console.log)
+      .then(({ message }) => mapBars(message))
+      .then(res => {
+        const context = canvasRef.current.getContext('2d')
+        context.canvas.width = WIDTH
+        context.canvas.height = HEIGHT
+        context.fillStyle = 'rgb(11, 13, 15)'
+        context.fillRect(0, 0, WIDTH, HEIGHT)
+      })
       .catch(e => {
         throw new Error(e)
       })
   }, [])
+
   return (
-    <header className="App-header">
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-    </header>
+    <body>
+      <canvas ref={canvasRef} />
+      {charts.flat().map((e, key) => (
+        <p key={key}>{e}</p>
+      ))}
+    </body>
   )
 }
 
